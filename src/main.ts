8ee48ac1055952060aaa7bc77e80b41ed2d9a894
stavguo <dmellogu@umich.edu>
2024-07-03 18:11:17 -0400

    updated game config to be better for pixel art

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,23 @@ import { Preloader } from './scenes/Preloader'
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+// As a reminder tiles are 16x16 pixels, 3:2 aspect ratio
+const screenScale = 4
+const widthPixels = 240
+const heightPixels = 160
 const config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
     backgroundColor: '#028af8',
+    pixelArt: true,
+    roundPixels: true,
     scale: {
+        parent: 'game-container',
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+        autoCenter: (window.innerWidth > window.innerHeight)
+            ? Phaser.Scale.CENTER_BOTH
+            : Phaser.Scale.CENTER_HORIZONTALLY,
+        width: screenScale * ((window.innerWidth > window.innerHeight) ? widthPixels : heightPixels),
+        height: screenScale * ((window.innerWidth > window.innerHeight) ? heightPixels : widthPixels),
     },
     scene: [
         Boot,
